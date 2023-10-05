d1c0c626d29313582371927b983c4c086626b330
Lav000 <valentin_thomas@icloud.com>
2023-10-05 16:54:35 +0200

    page d'accueil et dark mode

--- js/app.js
+++ js/app.js
@@ -3,3 +3,15 @@ document.addEventListener("DOMContentLoaded", e => {
     let el = document.querySelector("head");
     console.log(el.children[2].innerText = 'Javascript');
 });
+
+//dark mode
+const toggleDarkModeButton = document.getElementById('toggle-dark-mode');
+
+// Ajoutez un gestionnaire d'événements pour basculer le mode sombre
+toggleDarkModeButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
+
+document.addEventListener("DOMContentLoaded", e => {
+    var switchdarkmode = document.getElementById('toggle-dark-mode');
+});
