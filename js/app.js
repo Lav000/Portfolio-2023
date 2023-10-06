67622dbfa3471c18efc195b7c3301c75bf7c367a
Lav000 <valentin_thomas@icloud.com>
2023-10-06 10:34:06 +0200

    update script et réagencement du html

--- js/app.js
+++ js/app.js
@@ -2,16 +2,24 @@ document.addEventListener("DOMContentLoaded", e => {
     console.log(e.target);
     let el = document.querySelector("head");
     console.log(el.children[2].innerText = 'Javascript');
-});
 
-//dark mode
-const toggleDarkModeButton = document.getElementById('toggle-dark-mode');
+    //dark mode
+    const toggleDarkModeButton = document.getElementById('toggle-dark-mode');
 
-// Ajoutez un gestionnaire d'événements pour basculer le mode sombre
-toggleDarkModeButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-});
+    // Ajoutez un gestionnaire d'événements pour basculer le mode sombre
 
-document.addEventListener("DOMContentLoaded", e => {
-    var switchdarkmode = document.getElementById('toggle-dark-mode');
+    let x = 0;
+
+    toggleDarkModeButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        if (x == 0) {
+            x = 1;
+            document.querySelector("span").innerHTML = "&#x1F31E;";
+            console.log(!x);
+        } else {
+            x = 0;
+            document.querySelector("span").innerHTML = "&#x1F31D;";
+            console.log(x);
+        }
+    });
 });
