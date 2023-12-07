f857af7a89b09563153dcb10a1267c957f291d57
Lav000 <77846416+Lav000@users.noreply.github.com>
2023-12-07 23:44:41 +0100

    last update push in inc.php

--- include/header.inc.php
+++ include/header.inc.php
@@ -1,25 +1,19 @@
 <header>
-    <!-- Page de bienvenue -->
-    <section>
-        <button id="toggle-dark-mode" aria-label="dark mode">
-            <span id="vardarkmode" aria-hidden="true">
-            &#x1F31D;
-            </span>
-        </button>            
-    </section>
-
-    <!-- Page d'accueil -->
-    <section class="accueil-page">
-        <ul class="darkmode">
-            <li id="li-accueil">Accueil</li>
-            <li id="li-a_propos">À propos de moi</li>
-            <li id="li-competences">Compétences</li>
-            <li id="li-projets">Projets</li>
-            <!--<button id="toggle-dark-mode" aria-label="dark mode">
+        <!-- Page d'accueil -->
+        <section class="accueil-page">
+            <ul class="darkmode">
+                <li id="li-accueil">Accueil</li>
+                <li id="li-a_propos">À propos de moi</li>
+                <li id="li-competences">Compétences</li>
+                <li id="li-projets">Projets</li>
+            </ul>
+        </section>
+        <!-- Page de bienvenue -->
+        <section>
+            <button id="toggle-dark-mode" aria-label="dark mode">
                 <span id="vardarkmode" aria-hidden="true">
                 &#x1F31D;
                 </span>
-            </button> -->
-        </ul>
-    </section>
-</header>
+            </button>            
+        </section>
+    </header>
--- include/main.inc.php
+++ include/main.inc.php
@@ -11,11 +11,23 @@
 
         <!-- Page d'accueil -->
         <section class="accueil-page">
-            <img src="favicon/pdp.png" alt="photo de profil" loading="lazy">
-            <h1>Valentin Thomas</h1>
-            <p>
-                Dans la lueur du crépuscule, la ville s'éveille. Les rues résonnent du murmure des passants, les enseignes lumineuses clignotent hypnotiquement.
-                Au milieu du tumulte, des âmes variées se croisent, chacune avec son fardeau et son histoire. Les cafés embaument l'air de café frais, invitant à des moments précieux. Les gratte-ciel se dressent, sentinelles silencieuses dans cette symphonie de vie. C'est un tableau vibrant, où chaque personne contribue à la toile dynamique de l'existence quotidienne.
-            </p>
+            <section id="personnal">
+                <img src="src/img/pdp.png" alt="photo de profil" loading="lazy">
+                <h2>Valentin Thomas</h2>
+                <p>
+                    Dans la lueur du crépuscule, la ville s'éveille. Les rues résonnent du murmure des passants, les enseignes lumineuses clignotent hypnotiquement.
+                    Au milieu du tumulte, des âmes variées se croisent, chacune avec son fardeau et son histoire. Les cafés embaument l'air de café frais, invitant à des moments précieux. Les gratte-ciel se dressent, sentinelles silencieuses dans cette symphonie de vie. C'est un tableau vibrant, où chaque personne contribue à la toile dynamique de l'existence quotidienne.
+                </p>                
+            </section>
+            <section id="project">
+                <h2>
+                    Project
+                </h2>
+                <ul>
+                    <li>Projet Batman</li>
+                    <li>Projet portfolio</li>
+                    <li>Projet fuel calculator</li>
+                </ul>
+            </section>
         </section>
     </main>
